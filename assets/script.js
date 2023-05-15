9c4b45706986c5c354abc4e4ae5fe0d278c9710a
Chuck King <cking0987@gmail.com>
2023-05-14 22:16:56 -0400

    refactor to allow for multiple projects

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,140 @@
-/*
-TODO: 
+const projects = [];
 
--allow for multiple projects. That means scanning the img directory for unique project names first, then scoping all subsequent functions to that specific project. 
--better state management. each project should have initial,ready, and playing states. In initial, the controls should be hidden/disabled and there should be a clear button to click to load.
--add preload check to prev next buttons
-*/
-
-let tlProject = {
-	imgSet: []
+class Project {
+  constructor(name) { 
+		this.name = name;
+		this.imgSet = [];
+		this.currentIndex= 0;
+		this.isPlaying = false;
+	}
+	addImageToProject(image) {
+    this.imgSet.push(image);
+  }
+	calculateProjectMeta() {
+		this.nFrames = this.imgSet.length;
+		this.startDate = this.imgSet[0].dateStr;
+		this.endDate = this.imgSet[this.imgSet.length - 1].dateStr;
+		const durationInMs = new Date(this.endDate) - new Date(this.startDate);
+		this.durationInDays = durationInMs / ( 1000 * 60 * 60 * 24 );
+	}
+	addProjectToDOM() {
+		const projectEl = `
+			<div class="project-container initial" id="project-${this.name}">
+				<div class="project-meta">
+					<div id="project-name" class="meta-property">
+						<div class="label">project name:</div><div class="value">${this.name}</div>
+					</div>
+					<div id="start" class="meta-property">
+						<div class="label">start:</div><div class="value">${this.startDate}</div>
+					</div>
+					<div id="end" class="meta-property">
+						<div class="label">end:</div><div class="value">${this.endDate}</div>
+					</div>
+					<div id="duration" class="meta-property">
+						<div class="label">duration:</div><div class="value">${this.durationInDays} days</div>
+					</div>
+					<div id="frames" class="meta-property">
+						<div class="label">frames:</div><div class="value">${this.nFrames}</div>
+					</div>
+				</div>
+				<div class="slideshow-container" id="slideshow-${this.name}">
+					<figure>
+						<img src="${this.imgSet[0].filepath}" class="slide-img" id="img-${this.name}" alt="An image in a timelapse slideshow. The project name is ${this.name}">
+						<figcaption class="slide-name" id="cap-${this.name}">${this.imgSet[0].filename}</figcaption>
+					</figure>
+					<div class="ring">Loading
+						<span></span>
+					</div>
+				</div>
+				<div class="project-controls">
+					<button class="playpause" onClick="clickPlayPause('${this.name}')">play/pause</button> | 
+					<button class="prev" onClick="clickPrev('${this.name}')">prev</button> | 
+					<button class="next" onClick="clickNext('${this.name}')">next</button> | 
+					<button class="go-to-start" onClick="clickFirst('${this.name}')">first</button> | 
+					<button class="go-to-last" onClick="clickLast('${this.name}')">last</button>
+				</div>
+			</div>
+		`;
+		document.getElementById('projects-container').innerHTML += projectEl;
+		this.preloadImages();
+	}
+	preloadImages() {
+    this.imgSet.forEach((image) => {
+      let imgElement = new Image();
+      imgElement.src = image.filepath;
+    });
+  }
+	nextSlide() {
+		if (this.currentIndex < (this.nFrames -1)) {
+			this.currentIndex++;
+  		this.showSlide(this.currentIndex);
+		} else {
+			clearInterval(this.nextInterval);
+			this.isPlaying = false;
+		}
+	}
+	prevSlide() {
+		if (this.currentIndex > 0) {
+			this.currentIndex--;
+			this.showSlide(this.currentIndex);
+		}
+	}
+	showSlide(index) {
+		let imgEl = document.getElementById('img-' + this.name);
+		let capEl = document.getElementById('cap-' + this.name);
+		let currentImage = this.imgSet[index];
+		imgEl.src = currentImage.filepath;
+		capEl.textContent = currentImage.filename;
+	}
+	playPause() {
+		if (this.isPlaying) {
+			clearInterval(this.nextInterval);
+			this.isPlaying = false;
+		} else {
+			this.nextInterval = setInterval(() => this.nextSlide(), 100);
+			this.isPlaying = true;
+		}
+	}
+	goToStart() {
+		this.currentIndex = 0;
+		this.showSlide(0);
+	}
+	goToLast() {
+		this.currentIndex = (this.nFrames - 1);
+		this.showSlide(this.currentIndex);
+	}
 };
+
+function clickNext(projectName) {
+	let project = projects.find((proj) => proj.name === projectName);
+	if (project) {
+		project.nextSlide();
+	}
+}
+function clickPrev(projectName) {
+	let project = projects.find((proj) => proj.name === projectName);
+	if (project) {
+		project.prevSlide();
+	}
+}
+function clickPlayPause(projectName) {
+	let project = projects.find((proj) => proj.name === projectName);
+	if (project) {
+		project.playPause();
+	}
+}
+function clickFirst(projectName) {
+	let project = projects.find((proj) => proj.name === projectName);
+	if (project) {
+		project.goToStart();
+	}
+}
+function clickLast(projectName) {
+	let project = projects.find((proj) => proj.name === projectName);
+	if (project) {
+		project.goToLast();
+	}
+}
 
 function getImages() {
   var xhr = new XMLHttpRequest();
@@ -16,139 +142,36 @@ function getImages() {
   xhr.onload = function () {
     if (this.status == 200) {
       var images = JSON.parse(this.responseText);
-			buildTimelapseProjectDataObject(images);
+			parseImageList(images);
     }
   };
   xhr.send();
 }
-getImages();
 
-function buildTimelapseProjectDataObject(images) {
-	// This is just grabbing the project name from the first image in the list. This should be expanded to fist check for unique project names among all the files, then build a project data object for each #fixme
-	tlProject.name = images[0].split('/')[1].split('_')[0];
-	for (let i=0; i < images.length; i++) {
-		img = {};
+function parseImageList(images) {
+	// we expect an array of file paths that look like this
+	// img/pinus-strobus_2023-05-25_1800.webp
+	for (let i = 0; i < images.length; i++) {
+		let img = {};
 		img.filepath = images[i];
-		img.filename = images[i].split('/')[1];
-		imgMeta = images[i].split("_");
-		img.projectName = imgMeta[0].split("/")[1];
-		img.date = imgMeta[1];
-		img.time = imgMeta[2].split(".")[0];
-		tlProject.imgSet.push(img);
-	}
-	// Same with this info below. It's currently setting the values assuming there is only one tlProject. It should be expanded to run for each unique tlProject. 
-	tlProject.startDate = tlProject.imgSet[0].date;
-	tlProject.endDate = tlProject.imgSet.at(-1).date;
-	const durationInMs = new Date(tlProject.endDate) - new Date(tlProject.startDate);
-	tlProject.duration = durationInMs / (1000 * 60 * 60 * 24);
-	tlProject.nFrames = tlProject.imgSet.length;
-	addProjectToDOM(tlProject);
-}
-
-function addProjectToDOM(project) {
-	const projectEl = `
-		<div class="project-container initial" id="project-`+project.name+`">
-			<div class="project-meta">
-				<div id="project-name" class="meta-property">
-					<div class="label">project name:</div><div class="value">`+project.name+`</div>
-				</div>
-				<div id="start" class="meta-property">
-					<div class="label">start:</div><div class="value">`+project.startDate+`</div>
-				</div>
-				<div id="end" class="meta-property">
-					<div class="label">end:</div><div class="value">`+project.endDate+`</div>
-				</div>
-				<div id="duration" class="meta-property">
-					<div class="label">duration:</div><div class="value">`+project.duration+` days</div>
-				</div>
-				<div id="frames" class="meta-property">
-					<div class="label">frames:</div><div class="value">`+project.nFrames+`</div>
-				</div>
-			</div>
-			<div class="slideshow-container" id="slideshow-`+project.name+`">
-				<figure>
-					<img src="`+project.imgSet[0].filepath+`" class="slide-img" alt="An image in a timelapse slideshow. The project name is`+project.name+`">
-					<figcaption class="slide-name">`+project.imgSet[0].filename+`</figcaption>
-				</figure>
-				<div class="ring">Loading
-					<span></span>
-				</div>
-			</div>
-			<div class="project-controls">
-				<button onClick="startOver()">start over</button> |
-				<button onClick="previousSlide()">prev</button> | 
-				<button onClick="playPause()">play/pause</button> | 
-				<button onClick="nextSlide()">next</button>
-			</div>
-		</div>
-	`;
-	document.body.innerHTML += projectEl;
-}
-
-let currentIndex = 0;
-let isPlaying = false;
-let loadedCount = 0;
-let slideContainer, slideImage, slideFilename, imageCount, slideshowInterval, projectContainer;
-
-function preloadImages() {
-	projectContainer = document.getElementById('project-'+tlProject.name);
-	slideContainer = document.getElementById('slideshow-'+tlProject.name);
-	slideImage = slideContainer.querySelector('img');
-	slideFilename = slideContainer.querySelector('figcaption');
-	imageCount = tlProject.nFrames;
-	projectContainer.classList.remove('initial');
-	projectContainer.classList.add('loading');
-	for (let i = 0; i < imageCount; i++) {
-		const image = new Image();
-		image.onload = () => {
-			loadedCount++;
-			if (loadedCount === imageCount) {
-				projectContainer.classList.remove('loading');
-				projectContainer.classList.add('loaded', 'playing');
-				playPause();
-			}
-		};
-		image.src = tlProject.imgSet[i].filepath;
-	}
-}
-
-function showSlide(index) {
-	const currentImage = tlProject.imgSet[index];
-	slideImage.src = "/"+currentImage.filepath;
-	slideFilename.textContent = currentImage.filename;
-}
-
-function nextSlide() {
-	if (currentIndex < (imageCount-1) ) {
-		currentIndex = (currentIndex + 1) % imageCount;
-		showSlide(currentIndex);
-	} else {
-		playPause();
-	}
-}
-
-function previousSlide() {
-	currentIndex = (currentIndex - 1 + imageCount) % imageCount;
-	showSlide(currentIndex);
-}
-
-function startOver() {
-	currentIndex = 0;
-	showSlide(currentIndex);
-}
-
-function playPause() {
-	if (loadedCount === imageCount) {
-		if (isPlaying) {
-			clearInterval(slideshowInterval);
-			isPlaying = false;
-			projectContainer.classList.remove('playing');
-		} else {
-			slideshowInterval = setInterval(nextSlide, 150);
-			isPlaying = true;
-			projectContainer.classList.add('playing');
+		img.filename = img.filepath.split('/')[1];
+		img.dateStr = img.filename.split('_')[1];
+		img.timeStr = img.filename.split('_')[2].split('.')[0];
+		let projName = images[i].split('/')[1].split('_')[0];
+		let project = projects.find((proj) => proj.name === projName);
+		if (!project) {
+			project = new Project(projName);
+			projects.push(project);
 		}
-	} else {
-		preloadImages();
-	}
+		project.addImageToProject(img);
+	};
+	// once all the individual images have been parsed,
+	// calculate aggregate data about the projects themselves, 
+	// then add the project to the DOM
+	projects.forEach((project) => {
+		project.calculateProjectMeta();
+		project.addProjectToDOM();
+	});
 }
+
+getImages();
